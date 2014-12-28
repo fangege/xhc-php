f29f4eb97bfcf25b52c45b4fd429bfa05aef2c13
fangege <136994832@qq.com>
2014-12-28 22:42:43 +0800

    create my php git pro

---
+++ get_datas.php
@@ -0,0 +1,30 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+include("config.inc.php");
+include("dbconnect.php");
+include("function.inc.php");
+error_reporting(0);
+
+$type=$_GET["type"];
+$dev=$_GET["dev"];
+$dev_arr=array();
+if($type!=''){
+
+    if($type=="data"){
+		
+        $device_data = get_allDevice_data();
+        foreach($device_data as $k=>$v){
+                   if($dev==$v['device_name']){
+                     $dev_arr=$device_data[$k];
+                       break;
+                 }
+        }
+        $json=json_encode($dev_arr);
+//        print_r($dev_arr);
+        echo ($json);
+
+    }
+
+}
+
+
